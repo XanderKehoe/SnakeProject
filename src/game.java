9656c13ea2424c508d7bc1f7aa2fdb059a13252e
XanderKehoe <xanmail99@gmail.com>
2019-11-12 08:49:44 -0500

    first commit

---
+++ src/game.java
@@ -0,0 +1,15 @@
+import edu.utc.game.Scene;
+
+public class game implements Scene {
+	
+	public game() {
+		
+	}
+
+	@Override
+	public Scene drawFrame(int delta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/victory.java
@@ -0,0 +1,15 @@
+import edu.utc.game.Scene;
+
+public class victory implements Scene {
+	
+	public victory() {
+		
+	}
+
+	@Override
+	public Scene drawFrame(int delta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
